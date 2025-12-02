d91c406559bfe797fd924726628d07bf79b3a49d
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-12-02 08:23:52 -0600

    Added getStaticProps

---
+++ pages/index.tsx
@@ -0,0 +1,49 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+
+type Product = {
+  id: string;
+  title: string;
+  description: string;
+};
+
+function Home(props: { products: Product[] }) {
+  const { products } = props;
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const fileContent = await readFile(filePath, 'utf-8');
+  const data = JSON.parse(fileContent);
+
+  if(!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
+}
+
+export default Home;
